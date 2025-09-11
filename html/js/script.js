552f8b65591d7d12e2fbf7b0d0ca4f116fcd6a39
ikesmart <ikesmart06@gmail.com>
2025-09-11 08:19:31 +0100

    ipad responsiveness

--- html/js/script.js
+++ html/js/script.js
@@ -113,3 +113,10 @@ ScrollReveal().reveal('.home-content p, .about-content, .btn, .portfolio-bo, .hi
 
 
 
+// responsiveness
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
